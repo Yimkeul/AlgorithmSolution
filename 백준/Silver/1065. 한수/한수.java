aa0b4b7c20deee2e09b9b23babe88f2150437682
SEJUN LEE <leesjun29@gmail.com>
2023-07-14 01:32:18 +0900

    [Silver IV] Title: 한수, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = Integer.parseInt(br.readLine());
+		System.out.println(arithmetic_sequence(x));
+		
+	}
+	
+	public static int arithmetic_sequence(int num) {
+		int cnt = 0;
+		
+		if(num < 100) {
+			return num;
+		}else {
+			cnt = 99;
+			if(num == 1000	) {
+				num = 999;
+			}
+			
+			for(int i = 100; i <=num;i++) {
+				int hun = i /100;
+				int ten = (i/10)%10;
+				int one = i%10;
+				if(  (hun - ten) == (ten - one)) {
+					cnt++;
+				}
+			}
+		}
+		return cnt;
+		
+	}
+}
