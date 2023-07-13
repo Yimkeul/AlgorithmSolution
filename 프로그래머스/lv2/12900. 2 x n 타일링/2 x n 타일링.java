f564c517ce2d1baf151eb91ad3432da0d339fce8
SEJUN LEE <leesjun29@gmail.com>
2023-07-14 01:29:02 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.97 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+    int a = 1;
+    int b= 1;
+        for(int i = 0; i<n-1;i++){
+            int c= (a+b)% 1000000007;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+}
