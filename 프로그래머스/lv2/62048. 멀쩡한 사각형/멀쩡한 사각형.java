b4621891458c45faa7effa0822c3184f44ec53b5
SEJUN LEE <leesjun29@gmail.com>
2023-07-14 01:13:36 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.06 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+   public long solution(int w, int h) {
+        long answer = 0;
+       long min = (long)h;
+        long max= (long)w;
+        if(w<h){
+            min = (long)w;
+            max = (long)h;
+        }
+        long value = 1;
+        while(value >0){
+            value = max % min;
+            max= min;
+            min = value;
+            
+        }
+        answer= (long)w * (long)h - ((long)w+(long)h-max);
+        return answer;
+    }
+}
