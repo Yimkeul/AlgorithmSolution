8195d9032b7b99524df1db2bfacc8026621dcd17
SEJUN LEE <leesjun29@gmail.com>
2023-07-14 01:17:16 +0900

    [level 2] Title: H-Index, Time: 0.34 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int h;
+        int k;
+        Arrays.sort(citations);
+        for(int i = 0 ; i <citations.length;i++){
+         h = citations[i];
+            k =  citations.length - i;
+            if (k <= h) {
+                answer = k;
+                break;
+            }
+            
+        }
+        return answer;
+    }
+}
