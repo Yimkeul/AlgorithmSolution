d48c4580606ee69916ba94163d65b7656bb931a2
Yimkeul <leesjun29@gmail.com>
2023-11-13 09:47:45 +0900

    [level unrated] Title: 수열과 구간 쿼리 4, Time: 1.17 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
+    var ans:[Int] = arr
+    for query in queries {
+        let s = query[0]
+        let e = query[1]
+        let k = query[2]
+        
+        for i in s ... e {
+            if i % k == 0 {
+                ans[i] += 1
+            }
+        }
+        
+        // print(ans)
+        
+        
+    }
+    return ans
+}
