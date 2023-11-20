2cca0e51982671a3117d27d3049e4d08b8298d0b
Yimkeul <leesjun29@gmail.com>
2023-11-20 23:25:48 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.swift
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.swift
@@ -1,20 +1,15 @@
 import Foundation
 
 func solution(_ arr:[Int], _ query:[Int]) -> [Int] {
-    
     var result = arr
-    
     for i in 0 ..< query.count {
-        if i % 2 == 0{
-            // 짝수 인덱스 -> 인덱스 기준으로 뒷부분 버리고 앞부분 얻기
-            let index = query[i]
+        var index = query[i]
+        if i % 2 == 0 {
             result = Array(result.prefix(upTo: index + 1))
         } else {
-            // 홀수 인덱스 -> 인덱스 기준으로 앞부분 버리고 뒷부분 얻기
-            let index = query[i]
             result = Array(result.suffix(from: index))
         }
     }
-    
     return result
 }
+
