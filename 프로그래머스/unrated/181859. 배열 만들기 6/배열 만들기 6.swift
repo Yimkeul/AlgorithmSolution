7797e88dac5bd6af61c1e743abcaa098a3c8ab77
Yimkeul <leesjun29@gmail.com>
2023-11-28 21:32:14 +0900

    [level unrated] Title: 배열 만들기 6, Time: 9.97 ms, Memory: 19.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ arr:[Int]) -> [Int] {
+    var stk: [Int] = []
+    
+    for i in 0 ..< arr.count {
+        if stk.isEmpty {
+            stk.append(arr[i])
+        } else {
+            if stk[stk.count - 1] == arr[i] {
+                stk.popLast()
+            } else {
+                stk.append(arr[i])
+            }
+        }
+        
+    }
+    
+    return stk.isEmpty ? [-1] : stk
+}
