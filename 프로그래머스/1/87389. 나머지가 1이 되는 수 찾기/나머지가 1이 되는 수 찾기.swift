5d725e9c021e128aa6df58823aced44f587da9b8
Yimkeul <leesjun29@gmail.com>
2024-01-04 08:51:37 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var arr = [Int]()
+    
+    for i in 1 ..< n {
+        if n % i == 1 {
+            return i
+        }    
+    }
+    
+    return 0
+}
