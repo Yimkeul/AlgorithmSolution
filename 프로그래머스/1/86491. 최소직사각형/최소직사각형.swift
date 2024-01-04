c5a8294cc7dabcf8789a6bfa7caa743812a7949f
Yimkeul <leesjun29@gmail.com>
2024-01-04 21:58:11 +0900

    [level 1] Title: 최소직사각형, Time: 1.50 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -5,6 +5,7 @@ func solution(_ sizes: [[Int]]) -> Int {
     var maxHeight = 0
     
     for size in sizes {
+        
         maxWidth = max(maxWidth, min(size[0], size[1]))
         maxHeight = max(maxHeight, max(size[0], size[1]))
     }
