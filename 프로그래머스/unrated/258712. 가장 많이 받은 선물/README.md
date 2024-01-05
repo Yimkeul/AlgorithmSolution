079ae3144f48f69b38fca11662c5e4d3a540ac53
Yimkeul <leesjun29@gmail.com>
2024-01-05 09:08:25 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 8.79 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 16.6 MB, 시간: 8.93 ms
+메모리: 16.5 MB, 시간: 8.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 22:17:46
+2024년 1월 5일 9:8:22
 
 ### 문제 설명
 
--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.swift
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.swift
@@ -7,6 +7,7 @@ func solution(_ friends: [String], _ gifts: [String]) -> Int {
         for (index, friend) in friends.enumerated() {
             dict[friend] = index
         }
+    
 
         var intArray = [Int](repeating: 0, count: friends.count) // 선물 지수 배열
         var giftArrays = [[Int]](repeating: [Int](repeating: 0, count: friends.count), count: friends.count) // 선물 현황 배열 [준 사람, 받은 사람]
@@ -15,24 +16,21 @@ func solution(_ friends: [String], _ gifts: [String]) -> Int {
         for gift in gifts {
             let strs = gift.components(separatedBy: " ") // 0 : 준 사람, 1 : 받은 사람
             giftArrays[dict[strs[0]]!][dict[strs[1]]!] += 1
-            intArray[dict[strs[0]]!] -= 1
-            intArray[dict[strs[1]]!] += 1
+            intArray[dict[strs[0]]!] += 1
+            intArray[dict[strs[1]]!] -= 1
         }
 
         for i in 0..<intArray.count {
             var num = 0 // 선물 받은 개수
             for j in 0..<intArray.count where i != j {
                 // 선물을 받아야 하는 조건
-                if giftArrays[j][i] < giftArrays[i][j]
-                    || (giftArrays[j][i] == giftArrays[i][j] && intArray[i] < intArray[j]) {
+                if giftArrays[i][j] > giftArrays[j][i]
+                    || (giftArrays[i][j] == giftArrays[j][i] && intArray[i] > intArray[j]) {
                     num += 1
                 }
             }
 
-            // 최대 값이라면 최대 값 갱신
-            if answer < num {
-                answer = num
-            }
+            answer = max(answer, num)
         }
 
         return answer
