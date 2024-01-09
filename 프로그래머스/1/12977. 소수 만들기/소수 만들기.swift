7bbdf5991f814dfc6842caa56bb2fbdd9e5a12b3
Yimkeul <leesjun29@gmail.com>
2024-01-09 09:07:17 +0900

    [level 1] Title: 소수 만들기, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
@@ -0,0 +1,48 @@
+import Foundation
+
+func solution(_ nums:[Int]) -> Int {
+    var answer = 0
+    
+    var combiArray:[[Int]] = []
+    let _ = combi(nums, 3, 0, [], &combiArray)
+    
+    for i in combiArray {
+        if isDecimal(i) {
+            answer += 1
+        }
+    }
+    
+    return answer
+}
+
+func combi(_ array: [Int], _ pickCount: Int, _ index: Int, _ tempArray: [Int], _ combsArray: inout [[Int]]) -> [[Int]] {
+    if tempArray.count == pickCount {
+        combsArray.append(tempArray)
+        return []
+    }
+    
+    for i in index ..< array.count {
+        let _ = combi(array, pickCount, i + 1, tempArray + [array[i]], &combsArray)
+    }
+    return combsArray
+}
+
+
+func isDecimal(_ num: [Int]) -> Bool {
+    var sum = num.reduce(0,+)
+    if sum <= 1 {
+        return false
+    }
+    
+    if sum <= 3 {
+        return true
+    }
+    
+    let limit = Int(Double(sum).squareRoot()) + 1
+    for i in 2 ..< limit {
+        if sum % i == 0 {
+            return false
+        }
+    }
+    return true
+}
