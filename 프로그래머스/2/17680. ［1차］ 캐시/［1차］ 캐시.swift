dcd9d7bda0bc0d2192020a6235f420abb2a6379e
Yimkeul <leesjun29@gmail.com>
2024-01-26 10:48:50 +0900

    [level 2] Title: [1차] 캐시, Time: 4.35 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -11,14 +11,12 @@ func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
     for data in cities {
         if cache.contains(data) {
             // 캐시에 있는 경우 해당 인덱스를 pop 하고 cache에 푸쉬
-            guard let index = cache.firstIndex(of: data) else {
-                continue
-            }
+            let index = cache.firstIndex(of: data)!
             cache.remove(at: index)
             cache.append(data)
             time += 1
         } else {
-            // 캐시에 없는 경우 가장 하단 인덱스 pop 하고 cache에 푸쉬
+            // 캐시에 없는 경우 첫 인덱스 pop 하고 cache에 푸쉬
             cache.removeFirst()    
             cache.append(data)
             time += 5           
