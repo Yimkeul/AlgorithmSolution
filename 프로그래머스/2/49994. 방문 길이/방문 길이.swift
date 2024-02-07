dfdee26fb9f87bc353b0db322fabfa8b92cc9fc6
Yimkeul <leesjun29@gmail.com>
2024-02-07 16:28:23 +0900

    [level 2] Title: 방문 길이, Time: 11.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.swift
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.swift
@@ -1,26 +1,59 @@
+import Foundation
+
 func solution(_ dirs:String) -> Int {
-    var now = [0, 0]
-    var visited: Set<[Int]> = []
-
-    for dir in dirs {
-        var (dx, dy) = (0, 0)
-
-        switch dir {
-        case "U": (dx, dy) = (0, 1)
-        case "D": (dx, dy) = (0, -1)
-        case "L": (dx, dy) = (-1, 0)
-        case "R": (dx, dy) = (1, 0)
-        default: break
-        }
-
-        let next = [now[0]+dx, now[1]+dy]
-        if abs(next[0]) > 5 || abs(next[1]) > 5 { continue }
+            var visited: [(Int, Int, Int, Int)] = [(5,5,5,5)] // (x,y) 시작점 -> (x,y) 끝점 루트 자체를 저장하자.
         
-        if !visited.contains(now+next) && !visited.contains(next+now) {
-            visited.insert(now+next)
+        var previousPoint = (5,5)
+        var currentPoint = (5,5)
+        
+        var x = currentPoint.0
+        var y = currentPoint.1
+        
+        for dir in dirs {
+            switch dir {
+            case "U":
+                if y - 1 < 0 { // 판을 넘어가면
+                    continue
+                } else {
+                    y -= 1
+                }
+                break
+            case "L":
+                if x - 1 < 0 {
+                    continue
+                } else {
+                    x -= 1
+                }
+                break
+            case "R":
+                if x + 1 > 10 {
+                    continue
+                } else {
+                    x += 1
+                }
+                break
+            case "D":
+                if y + 1 > 10 {
+                    continue
+                } else {
+                    y += 1
+                }
+                break
+            default: print("Default")
+            }
+            
+            currentPoint = (x,y)
+            let check = (previousPoint.0, previousPoint.1, currentPoint.0, currentPoint.1)
+            let check2 = (currentPoint.0, currentPoint.1, previousPoint.0, previousPoint.1)
+            previousPoint = currentPoint
+            
+            if !visited.contains(where: {
+                return $0 == check || $0 == check2
+            }) {
+                visited.append(check)
+            }
         }
         
-        now = next
-    }
-    return visited.count
+        print(visited)
+        return visited.count - 1
 }
