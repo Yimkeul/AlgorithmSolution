b1249fc5e514c47011132c63f5e89771247cdc71
Yimkeul <leesjun29@gmail.com>
2024-02-13 09:31:11 +0900

    [level 2] Title: 택배상자, Time: 415.75 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func solution(_ order:[Int]) -> Int {
+    var beltStack: [Int] = []
+    var max: Int = 0
+    var count: Int = 0
+    
+    for box in order {
+        if beltStack.last == box {
+            count += 1
+            let _ = beltStack.removeLast()
+        } else if max < box {
+            count += 1
+            for b in max+1..<box {
+                beltStack.append(b)
+            }
+            max = box
+        } else {
+            break
+        }
+    }
+    return count
+}
