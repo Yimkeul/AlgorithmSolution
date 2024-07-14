ec701562ea61ffdcf384f7ffc39fd71a3221867f
Yimkeul <leesjun29@gmail.com>
2024-07-14 23:42:51 +0900

    [Gold IV] Title: 친구비, Time: 24 ms, Memory: 70020 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.swift
+++ 백준/Gold/16562. 친구비/친구비.swift
@@ -1,81 +1,79 @@
-func Q_16562() {
+func Q_16562_1() {
+
+    func find(_ x: Int) -> Int {
+        if parent[x] == x {
+            return x
+        }
+        parent[x] = find(parent[x])
+
+        return parent[x]
+    }
+
+    func union(_ x: Int, _ y: Int) {
+//        let fx = find(x)
+//        let fy = find(y)
+//        if fx < fy {
+//            parent[fy] = fx
+//        } else {
+//            parent[fx] = fy
+//        }
+        let rootX = find(x)
+        let rootY = find(y)
+        if rootX != rootY {
+            if rootX < rootY {
+                parent[rootY] = rootX
+            } else {
+                parent[rootX] = rootY
+            }
+        }
+    }
+
     let NMK = readLine()!.split { $0 == " " }.map { Int($0)! }
     let (N, M, K) = (NMK[0], NMK[1], NMK[2])
     let moneys = readLine()!.split { $0 == " " }.map { Int($0)! }
 
-    var grid = Array(repeating: [Int](), count: N)
-    var friendList = Array(repeating: false, count: N)
+    var parent = [Int](0..<N)
+//    var groups = Array(repeating: [Int](), count: N)
+    var groups = [Int: [Int]]()
+
 
     for _ in 0 ..< M {
         let VW = readLine()!.split { $0 == " " }.map { Int($0)! }
         let (V, W) = (VW[0] - 1, VW[1] - 1)
-        grid[V].append(W)
-        grid[W].append(V)
+        union(V, W)
     }
-    let connectedComponents = findConnectedComponents(grid)
+    
+//    for i in 0 ..< parent.count {
+//        groups[parent[i]].append(i)
+//    }
+//    print(parent)
+//    print(groups)
+    
+    for i in 0..<N {
+         let root = find(i)
+         if groups[root] == nil {
+             groups[root] = [Int]()
+         }
+         groups[root]!.append(i)
+     }
+
+
     var sum = 0
-    for i in 0 ..< connectedComponents.count {
-        var minValue = Int.max
-        for j in 0 ..< connectedComponents[i].count {
-            friendList[connectedComponents[i][j]] = true
-            minValue = min(minValue, moneys[connectedComponents[i][j]])
-        }
-        sum += minValue
-    }
+    var isFried = Array(repeating: false, count: N)
+    
+    for group in groups.values {
+         var minValue = Int.max
+         for node in group {
+             isFried[node] = true
+             minValue = min(minValue, moneys[node])
+         }
+         sum += minValue
+     }
 
-    if sum <= K {
-        print(sum)
-    } else {
+    if isFried.contains(false) || sum > K {
         print("Oh no")
-    }
-
-}
-
-fileprivate func find(_ parent: inout [Int], _ x: Int) -> Int {
-    if parent[x] != x {
-        parent[x] = find(&parent, parent[x])
-    }
-    return parent[x]
-}
-
-fileprivate func union(_ parent: inout [Int], _ rank: inout [Int], _ x: Int, _ y: Int) {
-    let rootX = find(&parent, x)
-    let rootY = find(&parent, y)
-
-    if rootX != rootY {
-        if rank[rootX] > rank[rootY] {
-            parent[rootY] = rootX
-        } else if rank[rootX] < rank[rootY] {
-            parent[rootX] = rootY
-        } else {
-            parent[rootY] = rootX
-            rank[rootX] += 1
-        }
+    } else {
+        print(sum)
     }
 }
-
-fileprivate func findConnectedComponents(_ graph: [[Int]]) -> [[Int]] {
-    let n = graph.count
-    var parent = Array(0..<n)
-    var rank = Array(repeating: 0, count: n)
-
-    for (node, neighbors) in graph.enumerated() {
-        for neighbor in neighbors {
-            union(&parent, &rank, node, neighbor)
-        }
-    }
-
-    var componentMap = [Int: [Int]]()
-
-    for i in 0..<n {
-        let root = find(&parent, i)
-        if componentMap[root] == nil {
-            componentMap[root] = [Int]()
-        }
-        componentMap[root]?.append(i)
-    }
-
-    return Array(componentMap.values)
-}
-
-Q_16562()
+Q_16562_1()
