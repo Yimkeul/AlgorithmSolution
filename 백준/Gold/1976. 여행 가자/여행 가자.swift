cdbf50cac4c4c361d2c92bb493847137fafd9bcd
Yimkeul <leesjun29@gmail.com>
2024-07-30 23:00:59 +0900

    [Gold IV] Title: 여행 가자, Time: 12 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.swift
+++ 백준/Gold/1976. 여행 가자/여행 가자.swift
@@ -1,36 +1,52 @@
-func Q_1976() {
-
-
-    let N = Int(readLine()!)!
+let N = Int(readLine()!)!
     let _ = Int(readLine()!)!
-    var graph: [[Int]] = []
-    var visited: [Bool] = Array(repeating: false, count: N)
-    var answer: Bool = true
     
-    for _ in 0 ..< N {
-        graph.append(readLine()!.split { $0 == " " }.map { Int($0)! })
-
-    }
-//    print(graph)
-    let goal = readLine()!.split { $0 == " " }.map { Int($0)! }
-    DFS(goal[0] - 1)
+    var parent = [Int](0 ... N)
+//    var root = Array(repeating: 0, count : N + 1)
     
-    for g in goal {
-        if !visited[g - 1] {
-            answer = false
-            break
+    func find(_ x:Int) -> Int {
+        if parent[x] == x {
+            return x
         }
+        parent[x] = find(parent[x])
+        return parent[x]
     }
-    print(answer ? "YES" : "NO")
-
-
-    func DFS(_ start: Int) {
-        visited[start] = true
-        for i in 0 ..< N {
-            if graph[start][i] == 1 && visited[i] == false {
-                DFS(i)
+    func union(_ x: Int, _ y: Int) {
+        let px = find(x)
+        let py = find(y)
+        if px == py { return }
+        if px < py {
+            parent[py] = px
+        } else {
+            parent[px] = py
+        }
+        
+    }
+    
+    for i in 1 ... N {
+        let line = readLine()!.split { $0 == " " }.map { Int($0)! }
+        for j in 0 ..< line.count {
+            if line[j] == 1 {
+                union(i, j + 1)
             }
         }
     }
-}
-let _ = Q_1976()
+    let plane = readLine()!.split { $0 == " " }.map { Int($0)! }
+    
+    var ans = "YES"
+    var root = find(plane[0])
+    
+//    for i in 0 ..< plane.count {
+//        if parent[plane[i]] != 1 {
+//            ans = "NO"
+//        }
+//    }
+//    print(parent)
+    
+    for city in plane {
+        if find(city) != root {
+            ans = "NO"
+            break
+        }
+    }
+    print(ans)
