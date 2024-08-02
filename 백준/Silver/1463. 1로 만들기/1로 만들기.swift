b03158b67605da9c224d4f64cd418863cd5e0cc7
Yimkeul <leesjun29@gmail.com>
2024-08-02 17:20:42 +0900

    [Silver III] Title: 1로 만들기, Time: 56 ms, Memory: 76916 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.swift
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -1,14 +1,13 @@
-let n = Int(readLine()!)!
-var dp = [Int](repeating: 0, count: n+1)
+    let N = Int(readLine()!)!
+var dp:[Int] = [Int](repeating: 0, count: N + 1)
+for i in 2 ..< N + 1 {
+        dp[i] = dp[i - 1] + 1
+        if i % 3 == 0 {
+            dp[i] = min(dp[i], dp[i / 3] + 1)
+        }
+        if i % 2 == 0 {
+            dp[i] = min(dp[i], dp[i / 2] + 1)
+        }
 
-for i in 2..<n+1 {
-    dp[i] = dp[i-1] + 1
-    if i % 3 == 0 {
-        dp[i] = min(dp[i], dp[i/3]+1)
     }
-    if i % 2 == 0 {
-        dp[i] = min(dp[i], dp[i/2]+1)
-    }
-}
-
-print(dp[n])
+    print(dp[N])
