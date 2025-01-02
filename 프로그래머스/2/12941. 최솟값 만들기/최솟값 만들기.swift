0670a400fba6e429e2831e947640e7e594b9e3e9
SeJun Lee <leesjun29@gmail.com>
2025-01-02 20:19:31 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -3,12 +3,14 @@ import Foundation
 func solution(_ A:[Int], _ B:[Int]) -> Int
 {
     var ans = 0
-    var A = A.sorted()
-    var B = B.sorted { $0 > $1}
-    for i in 0 ..< A.count {
-        ans += A[i] * B[i]
-    }
 
+    var sortedA = A.sorted { $0 > $1 }
+    var sortedB = B.sorted { $0 < $1 }
+    // print(sortedA, sortedB)
+    
+    for i in 0 ..< A.count {
+        ans += sortedA[i] * sortedB[i]
+    }
 
     return ans
 }
