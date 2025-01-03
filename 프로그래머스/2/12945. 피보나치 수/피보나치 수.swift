4564b720ef9797515cadf1723e14d948e494b485
SeJun Lee <leesjun29@gmail.com>
2025-01-03 16:16:41 +0900

    [level 2] Title: 피보나치 수, Time: 17.89 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.swift
@@ -1,19 +1,16 @@
-var memo = [Int: Int]()
-
 func solution(_ n: Int) -> Int {
-    return fibo(n) % 1234567
+    var memo = Array(repeating: -1, count: n + 1)
+    return fibo(n, &memo)
 }
 
-func fibo(_ n: Int) -> Int {
+func fibo(_ n: Int, _ memo: inout [Int]) -> Int {
     if n == 0 { return 0 }
-    else if n == 1 { return 1 }
-
-    if let result = memo[n] {
-        return result
-    }
-
-    let result = (fibo(n - 1) + fibo(n - 2)) % 1234567
-    memo[n] = result
+    if n == 1 { return 1 }
     
-    return result
+    if memo[n] != -1 {
+        return memo[n]
+    }
+    
+    memo[n] = (fibo(n - 1, &memo) + fibo(n - 2, &memo)) % 1234567
+    return memo[n]
 }
