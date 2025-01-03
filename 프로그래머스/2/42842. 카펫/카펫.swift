a16b1ea29f501af73554754d668da8c9b91b4401
SeJun Lee <leesjun29@gmail.com>
2025-01-03 20:08:24 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,53 +1,15 @@
 import Foundation
 
-func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    let total = brown + yellow
-    var hubo = [Int]()
-    var div = [Int]()
-    
-    var yx = [Int]()
-    var yy = [Int]()
-    var answer = [Int]()
-    
-    
-    for i in 1...total {
-        if total % i == 0 {
-            hubo.append(i)
-        }
-    }
-    
-    for i in 1...yellow {
-        if yellow % i == 0 {
-            div.append(i)
-        }
-    }
-    let len = div.count
-    if len % 2 == 1 {
-        for i in 0 ... len / 2 {
-            yy.append(div[i] + 2)
-        }
-        div = div.sorted {$0 > $1}
-        for i in 0 ... len / 2 {
-            yx.append(div[i] + 2)
-        }
-    } else {
-        for i in 0 ..< len / 2 {
-            yy.append(div[i] + 2)
-        }
-        div = div.sorted {$0 > $1}
-        for i in 0 ..< len / 2 {
-            yx.append(div[i] + 2)
-        }
-    }
+func solution(_ brown: Int, _ yellow: Int) -> [Int] {
+    let totalTiles = brown + yellow
 
-    for i in 0 ..< yx.count {
-        if hubo.contains(yx[i]) && hubo.contains(yy[i]) {
-            answer.append(yx[i])
-            answer.append(yy[i])
-            return answer
-            
+    for width in 3...totalTiles {
+        if totalTiles % width == 0 { 
+            let height = totalTiles / width
+            if (width - 2) * (height - 2) == yellow {
+                return [max(width, height), min(width, height)]
+            }
         }
     }
-    
-    return answer
+    return []
 }
