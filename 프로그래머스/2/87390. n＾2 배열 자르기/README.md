6e2237ceb9d1cd98f1e7abcf1868593440749a21
SeJun Lee <leesjun29@gmail.com>
2025-01-07 12:24:48 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.18 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
 
 ### 성능 요약
 
-메모리: 31.8 MB, 시간: 27.60 ms
+메모리: 34.4 MB, 시간: 6.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 9:44:23
+2025년 01월 07일 12:24:45
 
 ### 문제 설명
 
--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.swift
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.swift
@@ -1,12 +1,43 @@
+// import Foundation
+
+// func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
+//     var grid = [[Int]](repeating: [Int](repeating: 0, count: n), count :n)
+    
+    
+//     for i in 0 ..< n {
+//         for j in 0 ..< n {
+//             if i == j {
+//                 grid[i][j] = i + 1
+//             } else if i > j {
+//                 grid[i][j] = i + 1
+//             } else {
+//                 grid[i][j] = j + 1
+//             }
+//         }
+//     }
+    
+//     var RowArray = [Int]()
+    
+//     for i in grid {
+//         RowArray.append(contentsOf: i)
+//     }
+//     // print(RowArray)
+    
+//     return Array(RowArray[Int(left)...Int(right)])
+// }
+
+
+
 import Foundation
 
 func solution(_ n:Int, _ left:Int64, _ right:Int64) -> [Int] {
-    let result: [Int] = (Int(left)...Int(right)).map {
-        max($0 / n + 1, $0 % n + 1)
+    var result = [Int]()
+    
+    for index in Int(left)...Int(right) {
+        let row = index / n
+        let col = index % n
+        result.append(max(row, col) + 1)
     }
+    
     return result
 }
-
-
-
-
