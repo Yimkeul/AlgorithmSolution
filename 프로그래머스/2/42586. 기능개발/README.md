3dd0aee465deb652cd04d35d0f975d76d840f5d6
SeJun Lee <leesjun29@gmail.com>
2025-01-08 21:04:16 +0900

    [level 2] Title: 기능개발, Time: 0.13 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.08 ms
+메모리: 16.4 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 23:37:20
+2025년 01월 08일 21:04:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,27 +1,27 @@
 import Foundation
 
-func solution(_ progresses: [Int], _ speeds: [Int]) -> [Int] {
-    var answer: [Int] = []
-    var daysLeft: [Int] = []
-
-    // 각 기능의 남은 작업일 계산
-    for (progress, speed) in zip(progresses, speeds) {
-        daysLeft.append(Int(Foundation.ceil(Double(100 - progress) / Double(speed))))
+func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+    var ans = [Int]()
+    var queue = progresses.map { 100 - $0 }
+    
+    for i in 0 ..< speeds.count {
+        queue[i] = queue[i] % speeds[i] == 0 ? queue[i] / speeds[i] : (queue[i] / speeds[i]) + 1
     }
-
-    while !daysLeft.isEmpty {
-        // 첫 번째 기능의 작업일수
-        let deployDay = daysLeft.removeFirst()
-        var count = 1
-
-        // 다음 기능들 중 배포 가능한지 확인
-        while !daysLeft.isEmpty && deployDay >= daysLeft.first! {
-            daysLeft.removeFirst()
-            count += 1
+    while !queue.isEmpty {
+        let first = queue.first!
+        var cnt = 0
+        for p in queue {
+            if first >= p {
+                cnt += 1
+            } else {
+                break
+            }
         }
-
-        answer.append(count)
+        for _ in 0 ..< cnt {
+            queue.removeFirst()
+        }
+        ans.append(cnt)
     }
-
-    return answer
+    
+    return ans
 }
