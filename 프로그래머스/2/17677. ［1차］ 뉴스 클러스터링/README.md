62a45ecf4889bb8cdbe880686fcbd7931233f62c
SeJun Lee <leesjun29@gmail.com>
2025-01-09 23:26:46 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 29.11 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.44 ms
+메모리: 16.7 MB, 시간: 29.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 10:1:33
+2025년 01월 09일 23:26:41
 
 ### 문제 설명
 
--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.swift
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.swift
@@ -1,68 +1,109 @@
+// func solution(_ str1:String, _ str2:String) -> Int {
+//     var str1Array = [String]()
+//     var str1 = str1.map { String($0) }
+//     for i in 1 ..< str1.count {
+//         let str = str1[i - 1] + str1[i]
+//         str1Array.append(str.lowercased)
+//     }
+    
+//     var str2Array = [String]()
+//     var str2 = str2.map { String($0) }
+//     for i in 1 ..< str2.count {
+//         let str = str2[i - 1] + str2[i]
+//         str2Array.append(str.lowercased)
+//     }
+    
+//     str1Array = str1Array.filter { $0.count == 2 && $0.allSatisfy { $0.isLetter }}
+//     str2Array = str2Array.filter { $0.count == 2 && $0.allSatisfy { $0.isLetter }}
+    
+    
+//     var unionArray = [String]()
+    
+    
+//     var str2ArrayCopy = str2Array
+//     unionArray = str1Array.filter { element in  
+//         if let index = str2ArrayCopy.firstIndex(of: element) {
+//             str2ArrayCopy.remove(at: index)
+//             return true
+//         }
+//         return false
+//     }
+    
+//     let longerArray = str1Array.count > str2Array.count ? str1Array : str2Array
+//     let shorterArray = str1Array.count > str2Array.count ? str2Array : str1Array
+    
+//     var sumArray = longerArray
+    
+    
+//     for i in shorterArray {
+//         if !sumArray.contains(i) {
+//             sumArray.append(i)
+//         }
+//     }
+    
+//     let A:Double = unionArray.isEmpty ? 1.0 : Double(unionArray.count)
+//     let B:Double = sumArray.isEmpty ? 1.0 : Double(sumArray.count)
+    
+//     let answer = (A / B) * 65536
+//     return Int(answer)
+// }
+
 import Foundation
 
-func solution(_ str1:String, _ str2:String) -> Int {
-
-    var ans = 0
-    
-    // let str1 = isAlpa(str1.lowercased())
-    // let str2 = isAlpa(str2.lowercased())
-    let str1 = str1.lowercased()
-    let str2 = str2.lowercased()
-    // print(str1,str2)
-    
-    var ary1: [String] = []
-    for i in 0 ..< (str1.count - 1) {
-        let f = str1.index(str1.startIndex, offsetBy: i)
-        let s = str1.index(str1.startIndex, offsetBy: i + 1)
-        let w =  String(str1[f]) + String(str1[s])
-        if isAlpa(w).count == 2 {
-            ary1.append(w)
-        }
-           
+func solution(_ str1: String, _ str2: String) -> Int {
+    // 다중집합 생성 함수
+    func makeMultiSet(_ str: String) -> [String] {
+        var multiSet = [String]()
+        let lowerStr = str.lowercased()
         
-    }
-
-    var ary2: [String] = []
-    for i in 0 ..< (str2.count - 1) {
-        let f = str2.index(str2.startIndex, offsetBy: i)
-        let s = str2.index(str2.startIndex, offsetBy: i + 1)
-        let w =  String(str2[f]) + String(str2[s])
-        if isAlpa(w).count == 2{
-            ary2.append(w)    
-        }
-    }
-    
-    print(ary1, ary2)
-    var union = ary1.count + ary2.count
-    var intersection = 0
-    
-    for i in ary1.indices {
-        for j in ary2.indices {
-            if ary1[i] == ary2[j] {
-                intersection += 1
-                ary2.remove(at: j)
-                break
+        for i in 0..<lowerStr.count - 1 {
+            let first = lowerStr[lowerStr.index(lowerStr.startIndex, offsetBy: i)]
+            let second = lowerStr[lowerStr.index(lowerStr.startIndex, offsetBy: i + 1)]
+            if first.isLetter && second.isLetter { // 영문자만 유효
+                multiSet.append("\(first)\(second)")
             }
-         }
+        }
+        return multiSet
     }
-    
-    union -= intersection
-    // intersection / union
-    // if union == 0 {
-    //     return 65536
-    // } else {
-    //     ans = Int(Double(intersection) / Double(union) * 65536)
-    // }
-    
-    return union == 0 ? 65536 : Int(Double(intersection) / Double(union) * 65536)
-    
-    
-    return ans
-}
 
-func isAlpa(_ str: String) -> String {
-    let lowAlpaSet = CharacterSet.lowercaseLetters
-    let filters = str.components(separatedBy: lowAlpaSet.inverted).joined(separator: "")
-    
-    return filters
+    // 교집합 크기 계산
+    func intersection(_ set1: [String], _ set2: [String]) -> Int {
+        var set2Copy = set2
+        var count = 0
+        
+        for item in set1 {
+            if let index = set2Copy.firstIndex(of: item) {
+                count += 1
+                set2Copy.remove(at: index)
+            }
+        }
+        return count
+    }
+
+    // 합집합 크기 계산
+    func union(_ set1: [String], _ set2: [String]) -> Int {
+        var set1Copy = set1
+        var set2Copy = set2
+        
+        for item in set1 {
+            if let index = set2Copy.firstIndex(of: item) {
+                set2Copy.remove(at: index)
+            }
+        }
+        return set1Copy.count + set2Copy.count
+    }
+
+    // 다중집합 생성
+    let multiSet1 = makeMultiSet(str1)
+    let multiSet2 = makeMultiSet(str2)
+
+    // 교집합과 합집합 크기 계산
+    let interCount = intersection(multiSet1, multiSet2)
+    let unionCount = union(multiSet1, multiSet2)
+
+    // 자카드 유사도 계산
+    let jaccardSimilarity = unionCount == 0 ? 1.0 : Double(interCount) / Double(unionCount)
+
+    // 결과 반환 (65536 곱해서 정수로)
+    return Int(jaccardSimilarity * 65536)
 }
