87930ebb71398594b6111881224b1d1be0425f28
SeJun Lee <leesjun29@gmail.com>
2025-01-11 21:29:59 +0900

    [level 2] Title: 타겟 넘버, Time: 39.17 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -1,22 +1,23 @@
 import Foundation
 
-
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-  var count = 0
-  
-  func DFS(index: Int, sum: Int) {
-    if index == (numbers.count - 1) && sum == target { // 마지막 인덱스까지 모두 계산한 값이 target과 같다면
-      count += 1 // count를 +1해주고
-      return // 더 이상 계산할 값이 없으므로(자식노드가 없으므로) return 해준다
-    }
-
-    guard index + 1 < numbers.count else { return }
     
-    DFS(index: index+1, sum: sum + numbers[index + 1]) // +자식 노드 실행
-    DFS(index: index+1, sum: sum - numbers[index + 1]) // -자식 노드 실행
-  }
-  
-  DFS(index: -1, sum: 0)
-  
-  return count
+    var answer = 0
+    
+    func dfs(index: Int, currentSum: Int) {
+        if index == numbers.count {
+            if currentSum == target {
+                answer += 1
+            }
+            return 
+        }
+        
+        dfs(index: index + 1, currentSum: currentSum + numbers[index])
+        
+        dfs(index: index + 1, currentSum: currentSum - numbers[index])
+    }
+    dfs(index: 0, currentSum : 0)
+    
+    
+    return answer
 }
