dc7149561c9c78779eb012967151a7af93ee2b11
SeJun Lee <leesjun29@gmail.com>
2025-01-14 06:27:28 +0900

    [level 2] Title: 방문 길이, Time: 0.20 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,10 +1,10 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994#qna) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 11.11 ms
+메모리: 16.5 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 16:28:19
+2025년 01월 14일 06:27:25
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.swift
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.swift
@@ -1,59 +1,53 @@
 import Foundation
 
-func solution(_ dirs:String) -> Int {
-            var visited: [(Int, Int, Int, Int)] = [(5,5,5,5)] // (x,y) 시작점 -> (x,y) 끝점 루트 자체를 저장하자.
+struct Point: Hashable {
+    let x1: Int
+    let y1: Int
+    let x2: Int
+    let y2: Int
+    
+    init(_ x1: Int, _ y1: Int, _ x2: Int, _ y2: Int) {
+        // 두 점을 정렬하여 항상 같은 방향으로 저장
+        if x1 < x2 || (x1 == x2 && y1 < y2) {
+            self.x1 = x1
+            self.y1 = y1
+            self.x2 = x2
+            self.y2 = y2
+        } else {
+            self.x1 = x2
+            self.y1 = y2
+            self.x2 = x1
+            self.y2 = y1
+        }
+    }
+}
+
+func solution(_ dirs: String) -> Int {
+    var visited = Set<Point>()
+    var now = (0, 0)
+    
+    for d in dirs {
+        let (x, y) = now
+        var next = now
         
-        var previousPoint = (5,5)
-        var currentPoint = (5,5)
-        
-        var x = currentPoint.0
-        var y = currentPoint.1
-        
-        for dir in dirs {
-            switch dir {
-            case "U":
-                if y - 1 < 0 { // 판을 넘어가면
-                    continue
-                } else {
-                    y -= 1
-                }
-                break
-            case "L":
-                if x - 1 < 0 {
-                    continue
-                } else {
-                    x -= 1
-                }
-                break
-            case "R":
-                if x + 1 > 10 {
-                    continue
-                } else {
-                    x += 1
-                }
-                break
-            case "D":
-                if y + 1 > 10 {
-                    continue
-                } else {
-                    y += 1
-                }
-                break
-            default: print("Default")
-            }
-            
-            currentPoint = (x,y)
-            let check = (previousPoint.0, previousPoint.1, currentPoint.0, currentPoint.1)
-            let check2 = (currentPoint.0, currentPoint.1, previousPoint.0, previousPoint.1)
-            previousPoint = currentPoint
-            
-            if !visited.contains(where: {
-                return $0 == check || $0 == check2
-            }) {
-                visited.append(check)
-            }
+        switch d {
+        case "U": next = (x, y + 1)
+        case "D": next = (x, y - 1)
+        case "R": next = (x + 1, y)
+        case "L": next = (x - 1, y)
+        default: break
         }
         
-        print(visited)
-        return visited.count - 1
+        // 유효한 범위 확인
+        if isValid(next.0, next.1) {
+            visited.insert(Point(x, y, next.0, next.1))
+            now = next
+        }
+    }
+    
+    return visited.count
+}
+
+func isValid(_ x: Int, _ y: Int) -> Bool {
+    return x >= -5 && x <= 5 && y >= -5 && y <= 5
 }
