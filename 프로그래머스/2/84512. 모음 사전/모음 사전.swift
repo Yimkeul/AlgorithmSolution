8da482f12338e9974d6d59b0341dca9cb3150acd
SeJun Lee <leesjun29@gmail.com>
2025-01-15 01:14:55 +0900

    [level 2] Title: 모음 사전, Time: 0.08 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.swift
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.swift
@@ -1,20 +1,39 @@
 import Foundation
 
+// func solution(_ word:String) -> Int {
+//     let alpha = "AEIOU".map { String($0) }
+//     var result = [String]()
+    
+//     func dfs(_ str: String) {
+//         result.append(str)
+//         if str.count == 5 { return }
+//         for i in 0 ..< 5 {
+//             dfs(str + alpha[i])
+//         }
+//     }
+//     dfs("")
+//     return result.firstIndex(of: word)!
+// }
+
 func solution(_ word:String) -> Int {
-    let alpha = "AEIOU".map { String($0) }
-    var result = [String]()
+    let word = word.map { String($0) }
+    let dict = ["A": 1, "E": 2, "I": 3, "O": 4, "U": 5]
+    var nums = [1]
+    var sum = 0
 
-    func dfs(_ str: String) {
-        result.append(str)
-
-        if str.count == 5 { return }
-
-        for i in 0..<5 {
-            dfs(str + alpha[i])
+    for _ in 1..<5 {
+        if let num = nums.last {
+            nums.append(num * 5 + 1)
         }
     }
 
-    dfs("")
-    
-    return result.firstIndex(of: word)!
+    nums.reverse()
+
+    for i in 0..<word.count {
+        if let n = dict[word[i]] {
+            sum += nums[i] * (n-1) + 1
+        }
+    }
+
+    return sum
 }
