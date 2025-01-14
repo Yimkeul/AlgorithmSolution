365a57f435876c964d05f8fc303d1cef1a6225c3
SeJun Lee <leesjun29@gmail.com>
2025-01-15 06:04:47 +0900

    [level 2] Title: [3차] n진수 게임, Time: 180.78 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
@@ -1,25 +1,18 @@
-func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
-    var ary = [String]()
-    var start:Int = p - 1
-    var num = 0
-    var transNum: String = ""
-    
-    var temp:[Character] = []
-    while ary.count != t {
-        transNum = String(num, radix: n)
-        temp += Array(transNum)
+import Foundation
+
+    func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
+        var allResult = [String]()
+        var ans = ""
+        for i in 0 ..< t*m {
+            let tell = String(i, radix: n).map { String($0) }
+            allResult += tell
+        }
         
-        if temp.count > start {
-            if temp[start].isLetter {
-                ary.append(String(temp[start].uppercased()))    
-            } else {
-                ary.append(String(temp[start]))                
+        for i in 0 ..< allResult.count {
+            if i % m == (p - 1)  && ans.count < t{
+                ans += allResult[i].uppercased()
             }
-
-            start += m
-        } 
-            num += 1       
+        }
+        
+        return ans
     }
-    return ary.joined()
-}
-
