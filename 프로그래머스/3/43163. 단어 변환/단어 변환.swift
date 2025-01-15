de7469e9f5c8916974d5ed414fb60777c855102b
SeJun Lee <leesjun29@gmail.com>
2025-01-16 05:21:49 +0900

    [level 3] Title: 단어 변환, Time: 1.81 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -1,55 +1,36 @@
 import Foundation
 
-func solution(_ begin: String, _ target: String, _ words: [String]) -> Int {
-        var queue = [(word: String, depth: Int)]() // 단어와 변환 횟수를 저장하는 큐
-        var ischeck = [String: Bool]()
-
+func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
         if !words.contains(target) {
             return 0
         }
-
-        for word in words {
-            ischeck[word] = false
-        }
-
-        queue.append((begin, 0)) // 시작 단어와 깊이(0)를 큐에 추가
-
-        func bfs() -> Int {
-            while !queue.isEmpty {
-                let (currentWord, currentDepth) = queue.removeFirst()
-
-                // 타겟 단어에 도달하면 변환 횟수를 반환
-                if currentWord == target {
-                    return currentDepth
-                }
-
-                // 가능한 다음 단어들을 찾아 큐에 추가
-                for word in words {
-                    if !ischeck[word]! && canTransform(currentWord, word) {
-                        ischeck[word] = true
-                        queue.append((word, currentDepth + 1))
-                    }
-                }
-            }
-            return 0 // 타겟 단어에 도달할 수 없는 경우 0 반환
-        }
-
-        // 두 단어가 변환 가능한지 확인하는 함수 (한 글자만 다르면 변환 가능)
-        func canTransform(_ word1: String, _ word2: String) -> Bool {
-            let w1 = Array(word1)
-            let w2 = Array(word2)
-            var diffCount = 0
-
-            for i in 0..<w1.count {
-                if w1[i] != w2[i] {
-                    diffCount += 1
-                }
-                if diffCount > 1 {
-                    return false
-                }
-            }
+        
+        func isOneCharDiff( _ word1: String, _ word2: String) -> Bool {
+            let diffCount = zip(word1, word2).filter { $0 != $1 }.count
             return diffCount == 1
         }
-
-        return bfs()
+        
+        var queue: [(word: String, depth: Int)] = [(begin, 0)]
+        var visited = Set<String>()
+        visited.insert(begin)
+        
+        while !queue.isEmpty {
+            let (currentWord, depth) = queue.removeFirst()
+            if currentWord == target {
+                return depth
+            }
+            
+            for word in words {
+                if !visited.contains(word) && isOneCharDiff(currentWord, word) {
+                    queue.append((word, depth + 1))
+                    visited.insert(word)
+                }
+            }
+        }
+        
+        
+        
+        return 0
+        
+        
     }
