1feaf20981a4fc316df323fb3030ec45d0105701
SeJun Lee <leesjun29@gmail.com>
2025-01-22 19:32:56 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 40.39 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.swift
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.swift
@@ -1,26 +1,17 @@
 import Foundation
 
 func solution(_ numbers:[Int64]) -> [Int64] {
-    var result = [Int64]()
-    
-    for num in numbers{
-        if num % 2 == 0{
-            result.append(num + 1)
-        }else{
-            var binary = Array(String(num, radix: 2))
-            if binary[0] == Character("1"){
-                binary.insert(Character("0"), at: 0)
-            }
-            for idx in (0..<binary.count).reversed() {
-                if binary[idx] == Character("0"){
-                    binary[idx] = Character("1")
-                    binary[idx+1] = Character("0")
-                    break
-                }
-            }
-            result.append(Int64(String(binary), radix: 2)!)
-        }
+        var result = [Int64]()
+           
+           for number in numbers {
+               if number % 2 == 0 {
+                   result.append(number + 1)
+               } else {
+                   let bit = number ^ (number + 1)
+                   let answer = number + 1 + (bit >> 2)
+                   result.append(answer)
+               }
+           }
+           
+           return result
     }
-    
-    return result
-}
