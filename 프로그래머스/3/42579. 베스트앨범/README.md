72ff3df991e52226729be9539bb3178171d49e35
SeJun Lee <leesjun29@gmail.com>
2025-01-24 10:30:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.70 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#qna) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.31 ms
+메모리: 16.5 MB, 시간: 0.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 16일 05:52:06
+2025년 01월 24일 10:30:32
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -1,59 +1,36 @@
-// import Foundation
-
-// func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-//     var dics = [Int:(String,Int)]()
-//     var filterdics = [String:[(Int,Int)]]() // idx, plays
-//     let len = genres.count
-    
-//     for i in 0 ..< len {
-//         dics[i] = (genres[i], plays[i])
-//         filterdics[genres[i], default: []].append((i, plays[i]))
-//     }
-    
-    
-    
-//     print(dics)
-    
-//     var temp = filterdics.sorted {
-//         $0.map { (idx, _) in idx }.reduce(0,+) > $1.map { (idx, _) in idx }.reduce(0,+)
-//         // $0.map { $0[0] }.reduce(0,+) > $1.map { $0[0] }.reduce(0,+)
-//     }
-//     print(temp)
-    
-
-//     return []
-// }
-
 import Foundation
 
 func solution(_ genres: [String], _ plays: [Int]) -> [Int] {
-    var genreToPlayCount = [String: Int]()
-    var genreToSongs = [String: [(Int, Int)]]()
-    
-    let len = genres.count
-    
-    for i in 0 ..< len {
-        let genre = genres[i]
-        let play = plays[i]
-        
-        genreToPlayCount[genre, default: 0] += play
-        genreToSongs[genre, default: []].append((i, play))
-    }
-    
-    // 장르별 총 플레이 수로 정렬
-    let sortedGenres = genreToPlayCount.sorted { $0.value > $1.value }
-    
-    var result = [Int]()
-    
-    for (genre, _) in sortedGenres {
-        // 장르별 노래를 플레이 수로 내림차순 정렬
-        let songs = genreToSongs[genre]?.sorted { $0.1 > $1.1 } ?? []
-        
-        // 상위 2개의 노래를 선택
-        for i in 0 ..< min(2, songs.count) {
-            result.append(songs[i].0)
+        var ans = [Int]()
+        var gern_plays = [String: Int]()
+        for (index, value) in genres.enumerated() {
+            gern_plays[value, default: 0] += plays[index]
         }
+        let firstCondition = gern_plays.sorted { $0.1 > $1.1 }.map { $0.key }
+        
+        var each_gern_id_plays = [String:[(Int,Int)]]()
+        for (index, value) in genres.enumerated() {
+            each_gern_id_plays[value, default: []].append((index,plays[index]))
+        }
+        
+        for g in each_gern_id_plays {
+            var temp = g.value.sorted {
+                if $0.1 == $1.1 {
+                    return $0.0 < $1.0
+                } else {
+                    return $0.1 > $1.1
+                }
+            }
+            while temp.count > 2 {
+                temp.removeLast()
+            }
+            each_gern_id_plays[g.key] = temp
+        }
+
+        for i in firstCondition {
+            let ids = each_gern_id_plays[i]!.map { $0.0}
+            ans += ids
+        }
+        
+        return ans
     }
-    
-    return result
-}
