7a87ada12e8dda9d95bac735e874e00deff15d3c
SeJun Lee <leesjun29@gmail.com>
2025-01-25 04:37:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 376.21 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
@@ -1,45 +1,28 @@
-// import Foundation
-
-// func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-//     var queue = truck_weights
-//     var cnt = 0
-//     while !queue.isEmpty {
-//         var sumWeight = 0    
-//         for i in 0 ..< bridge_length {
-//             if sumWeight <= weight  &&  !queue.isEmpty {
-//                 sumWeight += queue.removeFirst()
-//                 cnt += 1
-//             }
-//                 cnt += 1                    
-//         }
-//     }
-    
-//     return cnt
-// }
-
 import Foundation
 
-func solution(_ bridge_length: Int, _ weight: Int, _ truck_weights: [Int]) -> Int { 
+func solution(_ bridge_length: Int, _ weight: Int, _ truck_weights: [Int]) -> Int {
     var time = 0
-    var bridgeQueue: [Int] = Array(repeating: 0, count: bridge_length)
-    var bridgeWeight = 0
-    var trucks = truck_weights
-    
-    while !bridgeQueue.isEmpty {
+    var bridgeQueue = Array(repeating: 0, count: bridge_length) // 다리 위 트럭 상태 관리
+    var currentWeight = 0 // 현재 다리 위 트럭의 총 무게
+    var truckIndex = 0 // 대기 트럭의 인덱스
+
+    while truckIndex < truck_weights.count {
+        // 1. 시간 경과
         time += 1
-        bridgeWeight -= bridgeQueue.removeFirst()
-        
-        if let truck = trucks.first {
-            if bridgeWeight + truck <= weight {
-                bridgeQueue.append(truck)
-                bridgeWeight += truck
-                trucks.removeFirst()
-            } else {
-                bridgeQueue.append(0)
-            }
+
+        // 2. 다리의 첫 번째 칸에서 트럭이 내려옴
+        currentWeight -= bridgeQueue.removeFirst()
+
+        // 3. 대기 중인 트럭이 다리에 올라갈 수 있는지 확인
+        if currentWeight + truck_weights[truckIndex] <= weight {
+            bridgeQueue.append(truck_weights[truckIndex]) // 트럭이 다리에 올라감
+            currentWeight += truck_weights[truckIndex]
+            truckIndex += 1
+        } else {
+            bridgeQueue.append(0) // 트럭이 올라가지 못하면 빈 칸 유지
         }
     }
-    
-    return time
-}
 
+    // 4. 마지막 트럭이 다리를 건너는 시간 추가
+    return time + bridge_length
+}
