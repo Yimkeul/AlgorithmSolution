eda117ca6c6c4fce91002db179aba90acdb88cb0
SeJun Lee <leesjun29@gmail.com>
2025-03-04 09:31:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 532 ms, Memory: 78492 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
@@ -1,46 +1,30 @@
-//
-//  main.swift
-//  solved
-//
-//  Created by yimkeul on 3/12/24.
-//
+let NM = readLine()!.split { $0 == " " }.map { Int($0)! }
+    let (N, M) = (NM[0], NM[1])
 
-import Foundation
+    var graph = [[Int]](repeating: [], count: N + 1)
+    var visited = [Bool](repeating: false, count: N + 1)
 
-let input = readLine()!.split { $0 == " " }.map { Int($0)! }
-let (n, m) = (input[0], input[1])
-var maps = Array(repeating: [Int](), count: n + 1)
-var visited = Array(repeating: false, count: n + 1)
-var ans = 0
-
-func dfs(start: Int) {
-    visited[start] = true
-    for i in 0 ..< maps[start].count {
-        let next = maps[start][i]
-        if visited[next] == false {
-            dfs(start: next)
+    for _ in 0 ..< M {
+        let UV = readLine()!.split { $0 == " " }.map { Int($0)! }
+        let (U, V) = (UV[0], UV[1])
+        graph[U].append(V)
+        graph[V].append(U)
+    }
+    
+    func dfs(_ node: Int) {
+        visited[node] = true
+        for next in graph[node] {
+            if !visited[next] {
+                dfs(next)
+            }
         }
     }
-}
-
-
-for _ in 0 ..< m {
-    let input = readLine()!.split { $0 == " " }.map { Int($0)! }
-    let (u, v) = (input[0], input[1])
-    maps[u].append(v)
-    maps[v].append(u)
-}
-
-for i in 1 ..< n + 1 {
-    if !visited[i] {
-        if maps[i].isEmpty {
-            ans += 1
-        } else {
-            dfs(start: i)
-            ans += 1
+    
+    var components = 0
+    for i in 1 ... N {
+        if !visited[i] {
+            dfs(i)
+            components += 1
         }
     }
-}
-
-print(ans)
-
+    print(components)
