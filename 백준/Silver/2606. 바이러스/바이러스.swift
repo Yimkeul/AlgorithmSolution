a02135b93532b52b2aeb827cec401f2391d19fae
SeJun Lee <leesjun29@gmail.com>
2025-03-05 10:09:30 +0900

    [Silver III] Title: 바이러스, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.swift
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -1,40 +1,29 @@
-//
-//  2606.swift
-//  solved
-//
-//  Created by yimkeul on 6/26/24.
-//
-
-import Foundation
-let _ = Q_2606()
-func Q_2606() {
-    let Computer = Int(readLine()!)! // 0 ... 100
+let n = Int(readLine()!)!
     let pair = Int(readLine()!)!
-    var answer = 0
-    var graph:[[Int]] = Array(repeating: Array(repeating: 0, count: Computer), count: Computer)
-    var isCheck:[Bool] = Array(repeating: false, count: Computer)
-    for _ in 0 ..< pair {
-        let input = readLine()!.split { $0 == " " }.map { Int($0)! }
-        let (B,A) = (input[0] - 1, input[1] - 1)
-        graph[A][B] = 1
-        graph[B][A] = 1
-    }
-    dfs(0, &graph, &isCheck)
-    print(answer - 1)
+    var virus = 0
     
-    func dfs(_ start:Int, _ graph: inout[[Int]], _ check: inout[Bool]) {
-        if check[start] {
-            return
-        }
+    var graph = [[Int]](repeating: [], count: n + 1)
+    var visited = [Bool](repeating: false, count: n + 1)
+    
+    
+    for _ in 0 ..< pair {
+        let uv = readLine()!.split { $0 == " "}.map { Int($0)! }
+        let (u,v) = (uv[0], uv[1])
+        graph[u].append(v)
+        graph[v].append(u)
+    }
+    
+    func dfs(_ node: Int) {
+        visited[node] = true
         
-        check[start] = true
-        answer += 1
-        for i in 0 ..< check.count {
-            if graph[start][i] == 1 && !check[i] {
-                dfs(i, &graph, &check)
+        for next in graph[node] {
+            if !visited[next] {
+                dfs(next)
+                virus += 1
             }
         }
-        
-        
     }
-}
+    
+    dfs(1)
+    
+    print(virus)
