8659b3df6f20aeb230bdfe14775ab95feafdf63b
SeJun Lee <leesjun29@gmail.com>
2025-03-13 10:14:46 +0900

    [Gold II] Title: 환승, Time: 464 ms, Memory: 98120 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.swift
+++ 백준/Gold/5214. 환승/환승.swift
@@ -1,51 +1,45 @@
-import Foundation
-
 let NKM = readLine()!.split { $0 == " " }.map { Int($0)! }
-let (N, K, M) = (NKM[0], NKM[1], NKM[2])
+    let (N, _, M) = (NKM[0], NKM[1], NKM[2])
 
-var graph = [[Int]](repeating: [], count: N + M + 1)
-var visited = [Bool](repeating: false, count: N + M + 1)
+    var graph = [[Int]](repeating: [], count: N + M + 1)
+    var visited = [Bool](repeating: false, count: N + M + 1)
 
-// 하이퍼튜브를 가상 노드로 추가
-for i in 1 ... M {
-    let input = readLine()!.split { $0 == " " }.map { Int($0)! }
-    let hyperTube = N + i  // 가상 노드 번호
-    
-    for station in input {
-        graph[station].append(hyperTube)
-        graph[hyperTube].append(station)
-    }
-}
-
-func bfs(_ start: Int) -> Int {
-    var queue = [(start, 1)] // (현재 역, 현재까지 방문한 역 개수)
-    var front = 0 // removeFirst() 최적화
-
-    visited[start] = true
-    
-    while front < queue.count {
-        let (node, count) = queue[front]
-        front += 1
-
-        if node == N {
-            return count
+    for i in 1 ... M {
+        let input = readLine()!.split { $0 == " " }.map { Int($0)! }
+        let hyperTube = N + i
+        
+        for station in input {
+            graph[station].append(hyperTube)
+            graph[hyperTube].append(station)
         }
-
-        for next in graph[node] {
-            if !visited[next] {
-                visited[next] = true
-                if next > N {
-                    // 하이퍼튜브 방문: 환승 없이 다음 역 이동
-                    queue.append((next, count))
-                } else {
-                    // 역 방문: 역 개수 증가
-                    queue.append((next, count + 1))
+    }
+    
+    func bfs(_ start: Int) -> Int {
+        var queue = [(start, 1)]
+        visited[start] = true
+        var index = 0
+        
+//        while !queue.isEmpty {
+        while index < queue.count {
+            let (node, depth) = queue[index]
+            index += 1
+            if node == N {
+                return depth
+            }
+            
+            for n in graph[node] {
+                if !visited[n] {
+                    visited[n] = true
+                    if n > N {
+                        queue.append((n, depth))
+                    } else {
+                        queue.append((n, depth + 1))
+                    }
+                    
                 }
             }
         }
+        
+        return -1
     }
-    
-    return -1
-}
-
-print(bfs(1))
+    print(bfs(1))
