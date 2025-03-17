b7102c770f3ab53dd353f2eb7b37d1d8ac9242b0
SeJun Lee <leesjun29@gmail.com>
2025-03-17 09:18:33 +0900

    [level 2] Title: 타겟 넘버, Time: 43.15 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -1,23 +1,62 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    
     var answer = 0
     
-    func dfs(index: Int, currentSum: Int) {
+    func dfs(_ index: Int, _ currentSum: Int) {
         if index == numbers.count {
             if currentSum == target {
                 answer += 1
             }
             return 
         }
+        dfs(index + 1,  currentSum + numbers[index])
+        dfs(index + 1,  currentSum - numbers[index])
         
-        dfs(index: index + 1, currentSum: currentSum + numbers[index])
-        
-        dfs(index: index + 1, currentSum: currentSum - numbers[index])
     }
-    dfs(index: 0, currentSum : 0)
+    print(dfs(0,0))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     return answer
 }
+
+
+// import Foundation
+
+// func solution(_ numbers:[Int], _ target:Int) -> Int {
+    
+//     var answer = 0
+    
+//     func dfs(index: Int, currentSum: Int) {
+//         if index == numbers.count {
+//             if currentSum == target {
+//                 answer += 1
+//             }
+//             return 
+//         }
+        
+//         dfs(index: index + 1, currentSum: currentSum + numbers[index])
+        
+//         dfs(index: index + 1, currentSum: currentSum - numbers[index])
+//     }
+//     dfs(index: 0, currentSum : 0)
+    
+    
+//     return answer
+// }
