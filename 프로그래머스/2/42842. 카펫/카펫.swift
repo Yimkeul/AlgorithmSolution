ea044cc643ec0e3a5f0a684594c2621eb9923273
SeJun Lee <leesjun29@gmail.com>
2025-03-31 09:20:55 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,15 +1,41 @@
 import Foundation
 
 func solution(_ brown: Int, _ yellow: Int) -> [Int] {
-    let totalTiles = brown + yellow
-
-    for width in 3...totalTiles {
-        if totalTiles % width == 0 { 
-            let height = totalTiles / width
+    let total = brown + yellow
+    
+    for width in 3 ... total {
+        if total % width == 0 {
+            let height = total / width
             if (width - 2) * (height - 2) == yellow {
-                return [max(width, height), min(width, height)]
+                return [max(width,height), min(width,height)]
             }
         }
     }
+    
     return []
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let totalTiles = brown + yellow
+
+//     for width in 3...totalTiles {
+//         if totalTiles % width == 0 { 
+//             let height = totalTiles / width
+//             if (width - 2) * (height - 2) == yellow {
+//                 return [max(width, height), min(width, height)]
+//             }
+//         }
+//     }
