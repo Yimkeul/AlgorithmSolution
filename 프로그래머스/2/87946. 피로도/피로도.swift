93d2c482512a13460bee4022e00dc65b528a8d70
SeJun Lee <leesjun29@gmail.com>
2025-04-02 09:13:00 +0900

    [level 2] Title: 피로도, Time: 27.64 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -1,24 +1,59 @@
 import Foundation
 
 func solution(_ k: Int, _ dungeons: [[Int]]) -> Int {
-    var maxCount = 0
+    var maxClear = 0
     var visited = [Bool](repeating: false, count: dungeons.count)
-
-    func dfs(_ fatigue: Int, _ count: Int) {
-        maxCount = max(maxCount, count)
-
-        for i in 0..<dungeons.count {
-            let required = dungeons[i][0]
-            let cost = dungeons[i][1]
-
-            if !visited[i] && fatigue >= required {
+    
+    func dfs(_ pirodo: Int, _ count: Int) {
+        maxClear = max(maxClear, count)
+        for i in 0 ..< dungeons.count {
+            let (need, use) = (dungeons[i][0], dungeons[i][1])
+            if !visited[i], pirodo >= need {
                 visited[i] = true
-                dfs(fatigue - cost, count + 1) 
-                visited[i] = false 
+                dfs(pirodo - use, count + 1)
+                visited[i] = false
             }
         }
     }
-
-    dfs(k, 0) 
-    return maxCount
+    
+    dfs(k,0)
+    
+    return maxClear
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     var maxCount = 0
+//     var visited = [Bool](repeating: false, count: dungeons.count)
+
+//     func dfs(_ fatigue: Int, _ count: Int) {
+//         maxCount = max(maxCount, count)
+
+//         for i in 0..<dungeons.count {
+//             let required = dungeons[i][0]
+//             let cost = dungeons[i][1]
+
+//             if !visited[i] && fatigue >= required {
+//                 visited[i] = true
+//                 dfs(fatigue - cost, count + 1) 
+//                 visited[i] = false 
+//             }
+//         }
+//     }
+
+//     dfs(k, 0) 
+//     return maxCount
