6b5922065903012e8fd68be3c840ba4e20e794af
SeJun Lee <leesjun29@gmail.com>
2025-04-07 09:29:52 +0900

    [level 3] Title: 베스트앨범, Time: 0.60 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.70 ms
+메모리: 16.7 MB, 시간: 0.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 10:30:32
+2025년 04월 07일 09:29:49
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -1,36 +1,100 @@
 import Foundation
 
 func solution(_ genres: [String], _ plays: [Int]) -> [Int] {
-        var ans = [Int]()
-        var gern_plays = [String: Int]()
-        for (index, value) in genres.enumerated() {
-            gern_plays[value, default: 0] += plays[index]
-        }
-        let firstCondition = gern_plays.sorted { $0.1 > $1.1 }.map { $0.key }
-        
-        var each_gern_id_plays = [String:[(Int,Int)]]()
-        for (index, value) in genres.enumerated() {
-            each_gern_id_plays[value, default: []].append((index,plays[index]))
-        }
-        
-        for g in each_gern_id_plays {
-            var temp = g.value.sorted {
-                if $0.1 == $1.1 {
-                    return $0.0 < $1.0
-                } else {
-                    return $0.1 > $1.1
-                }
-            }
-            while temp.count > 2 {
-                temp.removeLast()
-            }
-            each_gern_id_plays[g.key] = temp
-        }
-
-        for i in firstCondition {
-            let ids = each_gern_id_plays[i]!.map { $0.0}
-            ans += ids
-        }
-        
-        return ans
+    var data = [(Int,String,Int)]() // index, genres, plays
+    
+    for i in 0 ..< genres.count {
+        data.append((i,genres[i],plays[i]))
     }
+    
+    // 1
+    var setGenres = Array(Set(genres))
+    var totalPlaysForGenres = [(String,Int)]() //1
+    for i in 0 ..< setGenres.count {
+        let genre = setGenres[i]
+        let filtered = data.filter { $0.1 == genre }.map { $2 }.reduce(0,+)
+        totalPlaysForGenres.append((genre,filtered))
+    }
+    totalPlaysForGenres.sort { $0.1 > $1.1 }
+    
+    // 2
+    
+    var answer = [Int]()
+    for (genre, index)  in totalPlaysForGenres {
+        let filtered = data.filter { $0.1 == genre }
+        // 3 
+        let sortedData = filtered.sorted {
+            if $0.2 == $1.2 {
+                return $0.0 < $1.0 
+            } else {
+                return $0.2 > $1.2 
+            }
+        }
+        for i in 0 ..< sortedData.count where i <= 1 {
+            answer.append(sortedData[i].0)
+        }
+    }
+    
+
+    
+    return answer
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         var ans = [Int]()
+//         var gern_plays = [String: Int]()
+//         for (index, value) in genres.enumerated() {
+//             gern_plays[value, default: 0] += plays[index]
+//         }
+//         let firstCondition = gern_plays.sorted { $0.1 > $1.1 }.map { $0.key }
+        
+//         var each_gern_id_plays = [String:[(Int,Int)]]()
+//         for (index, value) in genres.enumerated() {
+//             each_gern_id_plays[value, default: []].append((index,plays[index]))
+//         }
+        
+//         for g in each_gern_id_plays {
+//             var temp = g.value.sorted {
+//                 if $0.1 == $1.1 {
+//                     return $0.0 < $1.0
+//                 } else {
+//                     return $0.1 > $1.1
+//                 }
+//             }
+//             while temp.count > 2 {
+//                 temp.removeLast()
+//             }
+//             each_gern_id_plays[g.key] = temp
+//         }
+
+//         for i in firstCondition {
+//             let ids = each_gern_id_plays[i]!.map { $0.0}
+//             ans += ids
+//         }
+        
+//         return ans
