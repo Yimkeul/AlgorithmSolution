5b5cda3bb3320edd5c28ad5e4e1c40e97bd8906a
Yimkeul <leesjun29@gmail.com>
2025-08-18 14:38:09 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,10 +1,15 @@
-x = int(input())
-y = int(input())
-if x > 0 and y > 0:
-    print(1)
-elif x > 0 and y < 0:
-    print(4)
-elif x < 0 and y > 0 :
-    print(2)
-elif x < 0 and y < 0:
-    print(3)
+import sys
+readline = sys.stdin.readline
+
+x = int(readline())
+y = int(readline())
+
+match (x, y):
+    case _ if x > 0 and y > 0:
+        print(1)
+    case _ if x < 0 and y > 0:
+        print(2)
+    case _ if x < 0 and y < 0:
+        print(3)
+    case _ if x > 0 and y < 0:
+        print(4)
