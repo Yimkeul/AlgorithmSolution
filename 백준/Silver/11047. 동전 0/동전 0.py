19a8085695999aa1d4122e3cfdab40f55406a78d
Yimkeul <leesjun29@gmail.com>
2025-08-26 17:58:15 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,16 +1,15 @@
-N,K= map(int,input().split())
-data = []
-for _ in range(N):
-    data.append(int(input()))
-
-data.sort(reverse=True)
-
-result = 0
-
-for i in data:
-    if K//i >0:
-        result += K//i
-        K = K%i
-
-
-print(result)
+import sys
+readline = sys.stdin.readline
+n, k = map(int, readline().split())
+money = [int(readline()) for _ in range(n)]
+money.reverse()
+cnt = 0
+for m in money:
+    if k == 0:
+        break
+    if k / n == 0:
+        continue
+    else:
+        cnt += k // m
+        k = k % m
+print(cnt)
