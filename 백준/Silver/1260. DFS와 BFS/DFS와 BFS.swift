151a1b150ee611932ed36e5716dfd65621d88778
Yimkeul <leesjun29@gmail.com>
2025-09-14 14:10:37 +0900

    [Silver II] Title: DFS와 BFS, Time: 20 ms, Memory: 69508 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
@@ -1,54 +1,43 @@
-//
-//  boj_1260.swift
-//  solved.ac
-//
-//  Created by yimkeul on 2/26/24.
-//
+let nmv = readLine()!.split { $0 == " "}.map { Int($0)! }
 
-import Foundation
+    var graph:[[Int]] = Array(repeating: [], count: nmv[0] + 1)
 
-let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
-let (N, M, V) = (input[0], input[1], input[2])
-var visited: [Bool] = Array(repeating: false, count: N + 1)
-var graph: [[Int]] = Array(repeating: [], count: N + 1)
+    var visited:[Bool] = Array(repeating: false, count: nmv[0] + 1)
 
-for _ in 0 ..< M {
-    let input = readLine()!.split { $0 == " " }.map { Int(String($0))! }
-    let (n1, n2) = (input[0], input[1])
-    graph[n1].append(n2)
-    graph[n2].append(n1)
-}
-graph = graph.map { $0.sorted() }
-
-func dfs(_ node: Int) {
-    visited[node] = true
-    print(node, terminator: " ")
-
-    for nextNode in graph[node] {
-        if !visited[nextNode] {
-            dfs(nextNode)
-        }
+    for _ in 0..<nmv[1] {
+        let xy = readLine()!.split { $0 == " "}.map { Int($0)! }
+        let (x,y) = (xy[0], xy[1])
+        graph[x].append(y)
+        graph[y].append(x)
     }
-}
+    graph = graph.map { $0.sorted() }
+    dfs(nmv[2])
+    visited = .init(repeating: false, count: nmv[0] + 1)
+    print()
+    bfs(nmv[2])
 
-func bfs(_ node: Int) {
-    var queue = [node]
-    var index = 0
-    visited[node] = true
-    while queue.count > index {
-        let currentNode = queue[index]
-        print(currentNode, terminator: " ")
-
-        for nextNode in graph[currentNode] {
-            if !visited[nextNode] {
-                visited[nextNode] = true
-                queue.append(nextNode)
+    func dfs(_ node: Int) {
+        visited[node] = true
+        print(node, terminator: " ")
+        for n in graph[node] {
+            if !visited[n] {
+                dfs(n)
             }
         }
-        index += 1
     }
-}
-dfs(V)
-visited = Array(repeating: false, count: N + 1)
-print()
-bfs(V)
+
+    func bfs(_ node: Int) {
+        var queue:[Int] = [node]
+        visited[node] = true
+
+        while !queue.isEmpty {
+            let first = queue.removeFirst()
+            print(first, terminator: " ")
+            for n in graph[first] {
+                if !visited[n] {
+                    visited[n] = true
+                    queue.append(n)
+                }
+            }
+        }
+    }
