2e5623bcea020fe2bc28b8993a4ef4e45ed920b0
Yimkeul <leesjun29@gmail.com>
2025-12-03 11:20:18 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,13 @@
-n = int(input())
+import sys
 
-if (n % 4 == 0 and n % 100 != 0) or n%400 ==0:
+readLine = sys.stdin.readline
+
+n = int(readLine())
+con1 = n % 4 == 0
+con2 = n % 100 != 0
+con3 = n % 400 == 0
+
+if con1 & (con2 | con3):
     print(1)
 else:
     print(0)
