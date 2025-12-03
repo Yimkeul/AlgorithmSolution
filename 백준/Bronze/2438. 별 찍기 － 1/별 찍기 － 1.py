00ca0272924fec3f0a613c67636eb5427e4e851b
Yimkeul <leesjun29@gmail.com>
2025-12-03 10:56:52 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -1,6 +1,13 @@
-n = int(input())
-for i in range (n):
-    for j in range(i+1):
-        print("*",end= "")
-    print()
+import sys
 
+readLine = sys.stdin.readline
+
+n = int(readLine())
+
+
+def drawstar(n: int):
+    for i in range(1, n + 1):
+        print("*" * i)
+
+
+drawstar(n)
