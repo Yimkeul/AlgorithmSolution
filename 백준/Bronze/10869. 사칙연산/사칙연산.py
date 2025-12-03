ed40cc14b529b687f2407de254d8efe74ceafd92
Yimkeul <leesjun29@gmail.com>
2025-12-03 14:55:30 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,4 +1,10 @@
-a,b = list(map(int,input().split(" ")))
+"""
+두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+"""
+import sys
+readLine = sys.stdin.readline
+a,b = map(int, readLine().split())
+
 print(a+b)
 print(a-b)
 print(a*b)
