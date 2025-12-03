d90e905e36f5c7bffce1ad2e643f91b858e913be
Yimkeul <leesjun29@gmail.com>
2025-12-03 14:51:49 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,14 +1,15 @@
 import sys
+
 readLine = sys.stdin.readline
-score = int(readLine())
-match score:
-    case _ if score >= 90:
-        print("A")
-    case _ if score >= 80:
-        print("B")
-    case _ if score >= 70:
-        print("C")
-    case _ if score >= 60:
-        print("D")
-    case _:
-        print("F")
+
+n = int(readLine())
+if n >= 90:
+    print("A")
+elif n >= 80:
+    print("B")
+elif n >= 70:
+    print("C")
+elif n >= 60:
+    print("D")
+else:
+    print("F")
