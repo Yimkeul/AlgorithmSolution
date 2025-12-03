298b9bdb781dfc407232dd4d2edd7e3df9212286
Yimkeul <leesjun29@gmail.com>
2025-12-03 11:12:12 +0900

    [Bronze V] Title: 구구단, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.py
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -1,3 +1,12 @@
-n = int(input())
-for i in range(1,10):
-    print(f"{n} * {i} = {n*i}")
+import sys
+
+
+def makegugudan(n: int):
+    for i in range(1, 10):
+        print(f"{n} * {i} = {n*i}")
+
+
+readLine = sys.stdin.readline
+n = int(readLine())
+
+makegugudan(n)
