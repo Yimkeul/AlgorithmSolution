7b9c6502d66ad66ccc4b73015e9a9c7059f25f35
Yimkeul <leesjun29@gmail.com>
2025-12-10 14:43:36 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,5 +1,5 @@
-arr = []
-for i in range(9):
-    arr.append(int(input()))
-print(max(arr))
-print(arr.index(max(arr))+1)
+import sys
+
+readLine = sys.stdin.readline
+nums = [int(readLine()) for _ in range(0, 9)]
+print(f"{max(nums)}\n{nums.index(max(nums)) + 1}")
