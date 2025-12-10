f022d7d2d01002eb23036a29b8410f1946c6569f
Yimkeul <leesjun29@gmail.com>
2025-12-10 16:01:03 +0900

    [Bronze III] Title: ACM 호텔, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,23 +1,12 @@
-def whereMyRoom (h, w, n):
-    str_floor = ""
-    str_room = ""
+import sys
 
-    floor = n%h
-    room = n//h + 1
-
+readLine = sys.stdin.readline
+T = int(readLine())
+for _ in range(0, T):
+    h, w, n = map(int, readLine().split())
+    floor = n % h
+    roomLine = (n // h) + 1
     if floor == 0:
         floor = h
-        room = n//h
-    if room < 10 :
-        str_room +="0"
-        str_room += str(room)
-    else:
-        str_room = str(room)
-    str_floor = str(floor)
-    return str_floor+str_room
-
-T = int(input())
-for _ in range(T):
-    H,W,N = map(int,input().split())
-    print(whereMyRoom(H,W,N))
-    
+        roomLine -= 1
+    print(floor * 100 + roomLine)
