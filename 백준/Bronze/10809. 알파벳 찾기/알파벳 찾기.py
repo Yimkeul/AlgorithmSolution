94b5f6ba851575d549fd43bf0bca98ff2e7027fe
Yimkeul <leesjun29@gmail.com>
2025-12-11 14:46:04 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,4 +1,8 @@
-S = input()
+import sys
 
-for x in 'abcdefghijklmnopqrstuvwxyz':
-    print(S.find(x), end = ' ')
+readLine = sys.stdin.readline
+S = readLine().rstrip()
+alpha = [-1] * 26
+for i in range(len(S)):
+    alpha[ord(S[i]) % 97] = i if alpha[ord(S[i]) % 97] == -1 else alpha[ord(S[i]) % 97]
+print(*alpha)
