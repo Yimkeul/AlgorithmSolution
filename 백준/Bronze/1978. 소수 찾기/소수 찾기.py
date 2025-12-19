2855270780a58d5b38b957a666f2b7a538e2a764
Yimkeul <leesjun29@gmail.com>
2025-12-19 17:05:14 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,14 +1,22 @@
-N = int(input())
-data = list(map(int,input().split()))
-ans =0
-for i in data:
-    tt=0
-    if i == 1:
-        continue
-    for j in range(1,i+1):
-        if i%j ==0:
-            tt+=1
-    if tt<=2:
-        ans+=1
+import sys
 
-print(ans)
+readLine = sys.stdin.readline
+N = int(readLine())
+nums = list(map(int, readLine().split()))
+cnt = 0
+
+
+def check(n: int):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
+
+
+for i in nums:
+    if check(i):
+        cnt += 1
+print(cnt)
