e8fb0fed4c3f1c5705c8984a610feee71513e4d0
Yimkeul <leesjun29@gmail.com>
2025-12-22 18:26:46 +0900

    [Bronze II] Title: 분해합, Time: 1512 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,16 @@
-n = int(input())
-result = 0
-for i in range(1, n+1):
-    nums = list(map(int, str(i)))
-    result = sum(nums) + i
-    if result == n:
-        print(i)
+import sys
+
+readLine = sys.stdin.readline
+
+N = int(readLine())
+ans = 0
+
+for i in range(N):
+    str_n = str(i)
+    temp_sum = 0
+    for t in str_n:
+        temp_sum += int(t)
+    if i + temp_sum == N:
+        ans = i
         break
-    if i == n:
-        print(0)
+print(ans)
