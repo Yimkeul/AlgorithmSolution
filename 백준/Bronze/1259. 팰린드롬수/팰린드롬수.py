8e15cd5f1f05338d389a32b41f1a59dde2147bb8
Yimkeul <leesjun29@gmail.com>
2025-12-29 23:33:53 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,21 +1,18 @@
-def isPalin(arr) :
-    half = len(arr)//2    
-    head = []
-    tail = []
-    for i in range(half):
-        head.append(arr[i])
-    if (len(arr)%2 == 0):
-        half -= 1
-    for i in range(len(arr)-1, half,-1):
-        tail.append(arr[i])
-    if (head == tail):
-        print("yes")
-    else:
-        print("no")
+import sys
+
+readLine = sys.stdin.readline
+
+
+def check(nums: str):
+    for i in range(len(nums) // 2):
+        if nums[i] != nums[len(nums) - 1 - i]:
+            print("no")
+            return
+    print("yes")
 
 
 while True:
-    num = list(input())
-    if len(num) == 1 and num[0]=="0":
+    input = readLine().rstrip()
+    if input == "0":
         break
-    isPalin(num)
+    check(input)
