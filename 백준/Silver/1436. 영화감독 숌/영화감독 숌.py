7596af548f093ae7fbd6f4535b0bf8bd0ceeb29f
Yimkeul <leesjun29@gmail.com>
2026-01-22 15:09:42 +0900

    [Silver V] Title: 영화감독 숌, Time: 636 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,11 +1,14 @@
-n = int(input())
-cnt = 0
-num = 1
-while True:
-    if "666" in str(num):
-        cnt += 1
-    if cnt == n:
-        print(num)
-        break
+import sys
+readline = sys.stdin.readline
 
-    num += 1
+N = int(readline().strip())
+
+cnt = 0
+result = 666
+while True:
+    if '666' in str(result):
+        cnt += 1
+    if cnt == N:
+        break
+    result += 1
+print(result)
